aa3b5426a1b9f7ee8099a4271077531ceda1d843
mortuusars <mortuusars@gmail.com>
2025-11-14 21:29:25 +0200

    Config

--- common/src/main/java/io/github/mortuusars/envelope/Config.java
+++ common/src/main/java/io/github/mortuusars/envelope/Config.java
@@ -39,16 +39,18 @@ public abstract class Config {
             {
                 builder.push("pigeon");
                 PIGEON_SPAWNS_NATURALLY = builder
-                      .comment(" Pigeon can spawn naturally in '#envelope:allows_pigeon_spawns' biomes. Default: true")
+                      .comment("Pigeon can spawn naturally in '#envelope:allows_pigeon_spawns' biomes.",
+                            " Default: true")
                       .define("spawns_naturally", true);
                 PIGEON_SPAWNS_IN_VILLAGE = builder
-                      .comment(" Pigeon can spawn in the village (similar to Cats). Default: true")
+                      .comment("Pigeon can spawn in the village (similar to Cats).",
+                            " Default: true")
                       .define("spawns_in_village", true);
                 PIGEON_MIN_TICKS_INSIDE_PIGEONHOLE = builder
-                      .comment(" Minimum time (in ticks) that a Pigeon will be sitting in the Pigeonhole after entering. Default: 600 (30 seconds)")
+                      .comment("Minimum time (in ticks) that a Pigeon will be sitting in the Pigeonhole after entering.")
                       .defineInRange("min_ticks_inside_pigeonhole", 600, 1, Integer.MAX_VALUE);
                 PIGEON_MIN_TICKS_OUTSIDE_PIGEONHOLE = builder
-                      .comment(" Minimum time (in ticks) that a Pigeon will spend outside of a Pigeonhole. Default: 1200 (1 min)")
+                      .comment("Minimum time (in ticks) that a Pigeon will spend outside of a Pigeonhole.")
                       .defineInRange("min_ticks_outside_pigeonhole", 1200, 100, Integer.MAX_VALUE);
                 builder.pop();
             }
@@ -56,25 +58,27 @@ public abstract class Config {
             {
                 builder.push("pigeonhole");
                 PIGEONHOLE_DISPENSER_WASTE_SCOOPING = builder
-                      .comment(" Waste from Pigeonhole can be scooped with dispenser that has a shovel (#envelope:waste_scoopable). Default: true.")
+                      .comment("Waste from Pigeonhole can be scooped with dispenser that has a shovel (#envelope:waste_scoopable).",
+                            " Default: true.")
                       .define("dispenser_waste_scooping", true);
                 PIGEONHOLE_ADDRESS_EXPERIENCE_LEVELS_COST = builder
-                      .comment(" Levels of experience needed to set or change an address. Default: 5")
+                      .comment("Levels of experience needed to set or change an address.")
                       .defineInRange("address_experience_levels_cost", 5, 0, 128);
                 builder.pop();
             }
 
             {
                 builder.push("letter");
-                LETTER_PAUSE = builder.comment(" Letter screen pauses singleplayer game. Default: false")
+                LETTER_PAUSE = builder.comment("Letter screen pauses singleplayer game.",
+                            " Default: false")
                       .define("pause", false);
                 builder.pop();
             }
 
             {
                 builder.push("package");
-                PACKAGE_PACK_LIMIT = builder.comment(" Number of packs that a single package can handle.",
-                            " (When reached, the item will be destroyed after all items are removed from it).", "Default: 3")
+                PACKAGE_PACK_LIMIT = builder.comment("Number of packs that a single package can handle.",
+                            "(When reached, the item will be destroyed after all items are removed from it).")
                       .defineInRange("pack_limit", 3, 1, Integer.MAX_VALUE);
                 builder.pop();
             }
@@ -82,13 +86,13 @@ public abstract class Config {
             {
                 builder.push("delivery");
                 DELIVERY_DEFAULT_DISTANCE = builder
-                      .comment(" Default distance (in blocks) that will be used if distance between two addresses cannot be determined (recipient does not exist, for example). Default: 1000")
+                      .comment("Default distance (in blocks) that will be used if distance between two addresses cannot be determined (recipient does not exist, for example).")
                       .defineInRange("default_distance", 1000, 1, Integer.MAX_VALUE);
                 DELIVERY_COURIER_TRAVEL_SPEED = builder
-                      .comment(" Courier speed (in blocks per second) while in traveling (background) phases. Default: 25.0")
-                      .defineInRange("courier_travel_speed", 25.0, 0.0001, 9999.0);
+                      .comment("Courier speed (in blocks per second) while in traveling (background) phases.")
+                      .defineInRange("courier_travel_speed", 25.0, 0.01, 9999.0);
                 DELIVERY_TRAVEL_DURATION_DISTANCE_CAP = builder
-                      .comment(" Distance (in blocks) after which travel duration stops increasing and stays at maximum value. Default: 5000")
+                      .comment("Distance (in blocks) after which travel duration stops increasing and stays at maximum value.")
                       .defineInRange("travel_duration_distance_cap", 5000, 1, Integer.MAX_VALUE);
                 builder.pop();
             }
@@ -96,7 +100,8 @@ public abstract class Config {
             {
                 builder.push("debug");
                 DEBUG = builder
-                      .comment(" Enable debug features. Will affect performance negatively. Don't enable unless it's needed. Default: false.")
+                      .comment("Enable debug features. Will affect performance negatively. Don't enable unless it's needed.",
+                            " Default: false.")
                       .define("debug_mode", false);
                 builder.pop();
             }
